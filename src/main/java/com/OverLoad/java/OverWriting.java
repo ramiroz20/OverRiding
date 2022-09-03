f38b4ac75d9743577881bce01f57abf94a5876a7
ramiroz20 <Ramiroz94@mail.ru>
2022-09-03 22:09:42 +0500

    Overriding

---
+++ src/main/java/com/OverLoad/java/OverWriting.java
@@ -0,0 +1,28 @@
+package com.OverLoad.java;
+
+
+    class Parent {
+
+        int method() {
+
+            return 1;
+        }
+
+        class Child extends Parent {
+            @Override
+            int method() {
+                return 2;
+            }
+
+        }
+            public class OverWriting {
+
+                public static void main(String[] args) {
+                    Parent parent = new Parent();
+                    Parent child = new Child();
+
+                    System.out.println(parent.method());
+                    System.out.println(child.method());
+                }
+            }
+        }
